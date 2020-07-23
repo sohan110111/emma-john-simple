0f2c77296ae0eb11e52e8bf33c0f7e592e8bdcad
sohan110111 <sohan110111@gmail.com>
2020-07-24 04:00:47 +0600

    fontAwesom install event handler in add to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
 
 const Product = (props) => {
-    console.log(props.product.name);
+    //console.log(props);
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h3>This is Product</h3>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button
+                 className = "main-button"
+                 onClick = {() => props.handleAddProduct(props.product)}
+                 ><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+            </div>
+
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,11 +6,18 @@ import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts]= useState(first10);
+
+    const handleAddProduct = (product) => {
+        console.log("product added", product);
+    }
     return (
         <div className = "shop-container">
             <div className = "product-container">
                 {
-                    products.map(pd => <Product product={pd}></Product>)
+                    products.map(pd => <Product 
+                        handleAddProduct = {handleAddProduct}
+                        product={pd}
+                        ></Product>)
                 }
             </div>
             <div className = "cart-container">

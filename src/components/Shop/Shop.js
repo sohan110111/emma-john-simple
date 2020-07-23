dfea5af36c7b3715a4b100276701c3a913654ab5
sohan110111 <sohan110111@gmail.com>
2020-07-23 18:50:58 +0600

    product component send dynamic data by props

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import './Shop.css';
+import Product from '../Product/Product';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts]= useState(first10);
     return (
-        <div>
-            <h1>This is shop.</h1>
-            <h3>{products.length}</h3>
-            <ul>
+        <div className = "shop-container">
+            <div className = "product-container">
                 {
-                    products.map(product => <li>{product.name}</li>)
+                    products.map(pd => <Product product={pd}></Product>)
                 }
-            </ul>
+            </div>
+            <div className = "cart-container">
+                <h3>This is cart.</h3>
+            </div>
         </div>
     );
 };

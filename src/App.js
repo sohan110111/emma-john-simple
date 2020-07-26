c39673e86b573406d49450712d19fb59a60e43d5
sohan110111 <sohan110111@gmail.com>
2020-07-26 15:53:22 +0600

    react route dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
---
+++ src/fakeData/foods.js
@@ -0,0 +1,19 @@
+var foods = [
+    {
+        name = 'Biryani',
+        price = 200,
+        ingredients = ['rice', 'chicken']
+    },
+    {
+        name = 'Rosmalai',
+        price = 500,
+        ingredients = ['ros', 'malai']
+    },
+    {
+        name = 'Biryani',
+        price = 200,
+        ingredients = ['rice', 'chicken']
+    }
+]
+
+export default foods;

120f1595e5fa0148cd347a15929f640eb1df1f47
sohan110111 <sohan110111@gmail.com>
2020-07-26 23:12:38 +0600

    react rout dom

--- src/App.js
+++ src/App.js
@@ -8,12 +8,37 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+             <Review></Review>
+          </Route>
+          <Route path="/manage">
+              <Inventory></Inventory>
+          </Route>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/product/:productKey">
+            <ProductDetail></ProductDetail>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
       
     </div>
   );
---
+++ src/components/ProductDetail/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetail = () => {
+    return (
+        <div>
+            <h1>ProductDetail is comming soon.</h1>
+        </div>
+    );
+};
+
+export default ProductDetail;

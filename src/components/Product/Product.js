e6060cc1578a6902e34c8cdbed9ded1dcb429069
sohan110111 <sohan110111@gmail.com>
2020-07-31 04:47:59 +0600

    Load data based on route parameter, conditional formating

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,7 +5,7 @@ import './Product.css';
 import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-    console.log(props.product.key);
+    console.log(props);
     const { img, name, seller, price, stock, key } = props.product;
     return (
         <div className="product">
@@ -19,10 +19,10 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>Only {stock} left in stock - Order soon</small></p>
-                <button
-                 className = "main-button"
-                 onClick = {() => props.handleAddProduct(props.product)}
-                 ><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>
+            {   props.showAddToCart === true && <button
+                className = "main-button"
+                onClick = {() => props.handleAddProduct(props.product)}
+                ><FontAwesomeIcon icon={faShoppingCart} />add to cart</button>}
             </div>
 
         </div>

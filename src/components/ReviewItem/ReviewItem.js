d7391c972faa2aca99831f5a3e4a917561e254fe
sohan110111 <sohan110111@gmail.com>
2020-07-31 21:59:08 +0600

    Review component, Review items, css in javaScript style

---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    //console.log(props);
+    const {name, quantity} = props.product;
+    const ReviewItemStyle={
+        borderBottom:'1px solid lightgray',
+        marginBottom:'5px',
+        paddingBottom:'5px',
+        marginLeft:'200px'
+    };   
+    return (
+        <div style={{borderBottom:'1px solid lightgray'}} className="review-item">
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity: {quantity}</p>
+            <br/>
+            <button className="main-button">Remove </button>
+        </div>
+    );
+};
+
+export default ReviewItem;
